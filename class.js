87ea3dfb13a874e718ec0bd7f32ff17fde35164c
gjy_1992 <gjy_1992@qq.com>
2021-02-16 00:11:22 +0800

    定义一些基础类的格式

--- class.js
+++ class.js
@@ -73,3 +73,45 @@ class Planet{
     }
 
 }
+
+/* 资源 */
+class Resources{
+    constructor(name, basic=true, rare=false, compound=false) {
+        /* 名字，用于索引 */
+        this.name = name;
+        /* 是否为基础资源 */
+        this.isBasic=basic;
+        /* 是否为稀有资源 */
+        this.isRare = rare;
+        /* 是否为合成产物 */
+        this.isCompound = compound;
+    }
+}
+
+/* 配方 */
+class Recipe{
+    constructor(tag, inputs, outputs, time)
+    {
+        /* 名称标记，暂时用于识别 */
+        this.tag = tag;
+        /* 输入产物字典 */
+        this.inputs = inputs;
+        /* 输出产物字典 */
+        this.outputs = outputs;
+        /* 基础消耗时间 */
+        this.time = time;
+    }
+}
+
+/* 机器 */
+class Machine{
+    constructor(name, recipes, power)
+    {
+        /* 名称 */
+        this.name = name;
+        /* 支持的配方 */
+        this.recipes = recipes;
+        /* 额定功率 */
+        this.power = power;
+    }
+}
